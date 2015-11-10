fd069d2ab48e1914bc7948f33f75a83ff896f50a
Bystrov Aleksandr <a.bystrov@rambler-co.ru>
2015-11-10 16:58:55 +0300

    Fix for retina

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
-var fs = require('fs');
-var path = require('path');
-var Sprite = require('./lib/sprite-webpack')
-var _ = require('lodash')
+var Sprite = require('./lib/sprite-webpack');
+var _ = require('lodash');
 
 function SpriteWebpackPlugin(options) {
   var opt = Sprite.options;
   this.options = _.assign(opt, options);
 }
 
-SpriteWebpackPlugin.prototype.apply = function(compiler) {
+SpriteWebpackPlugin.prototype.apply = function() {
   var self = this;
   var opt = self.options;
+  var newOpt = _.clone(opt, true);
+
+  newOpt = _.assign(newOpt, {
+    source: opt.source + 'scaled-at-200/',
+    spriteName: opt.spriteName + '@2x'
+  });
+
   Sprite.createStyles(opt);
   Sprite.createImage(opt);
+  Sprite.createImage(newOpt);
   Sprite.addImport(opt);
-}
+};
 
 module.exports = SpriteWebpackPlugin;
